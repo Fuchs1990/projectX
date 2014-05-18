27b500f8f3231abd80f602326afa3b327f4fe053
Fuchs1990 <saschamaekler@yahoo.de>
2014-05-18 13:17:14 +0200

    test

---
+++ src/main/java/de/name/projectX/App.java
@@ -0,0 +1,13 @@
+package de.name.projectX;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
